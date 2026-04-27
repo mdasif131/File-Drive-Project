8f0241233f1d955cf1af26b5a056578b0963e97b
mdasif131 <asifmdasifchy@gmail.com>
2026-04-28 03:04:28 +0600

    light and dark mode setup

--- components/web/Header/Header.tsx
+++ components/web/Header/Header.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { ThemeToggle } from "@/components/theme-toggle"
 import { Button } from "@/components/ui/button"
 import { OrganizationSwitcher, SignInButton, UserButton } from "@clerk/nextjs"
 import { Authenticated, Unauthenticated } from "convex/react"
@@ -8,18 +9,22 @@ const Header = () => {
     <div className="border-b bg-muted py-4">
       <div className="container mx-auto flex justify-between">
         <h1 className="text-xl font-bold">
-          File<span className="text-primary font-black tracking-wide">Drive</span>
+          File
+          <span className="font-black tracking-wide text-primary">Drive</span>
         </h1>
-        <div className="flex gap-2">
-          <Authenticated>
-            <OrganizationSwitcher />
-            <UserButton />
-          </Authenticated>
-          <Unauthenticated>
-            <SignInButton mode="modal">
-              <Button>Sign in</Button>
-            </SignInButton>
-          </Unauthenticated>
+        <div className="flex items-center gap-2">
+          <div className="flex gap-2">
+            <Authenticated>
+              <OrganizationSwitcher />
+              <UserButton />
+            </Authenticated>
+            <Unauthenticated>
+              <SignInButton mode="modal">
+                <Button>Sign in</Button>
+              </SignInButton>
+            </Unauthenticated>
+          </div>
+          <ThemeToggle/>
         </div>
       </div>
     </div>
