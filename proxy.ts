b1f604201ab9acf64b11518a4fb328a3ec3ab7cf
mdasif131 <asifmdasifchy@gmail.com>
2026-05-03 13:44:17 +0600

    end

--- proxy.ts
+++ proxy.ts
@@ -3,9 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 const isProtectedRoute = createRouteMatcher(["/files(.*)", "/dashboard(.*)"])
 
 export default clerkMiddleware((auth, req) => {
-   console.log("middleware hit:", req.nextUrl.pathname)
   if (isProtectedRoute(req)) {
-    auth.protect() //you redirect model:signIn"
+    auth.protect() 
   }
 })
 
